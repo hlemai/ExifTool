ff57d667f4427692aed7e971c6f9dcac484e2cb9
Hervé LEMAI <hlemai@hotmail.com>
2021-05-02 21:44:50 +0200

    test with current path

--- Sources/ExifTool/ExifTool.swift
+++ Sources/ExifTool/ExifTool.swift
@@ -1,9 +1,21 @@
 import Foundation
 
+@available(macOS 10.13, *)
 public class ExifTool {
     
-    static var exifToolPath = "/opt/homebrew/bin/exiftool"
-    
+    //static var exifToolPath = "/opt/homebrew/bin/exiftool"
+    private static var exifToolPath = "/Users/hlemai/Dev/next/common/ext/exiftool/exiftool"
+
+    public static func read(fromurl:URL) -> ExifTool {
+        let exif = ExifTool(filepath:fromurl.path)
+        exif.fillMetataData()
+        return exif
+    }
+
+    public static func setExifTool(_ path:String) {
+        exifToolPath = path
+    }
+
     let filepath:String
 
     var metadatas:[String:String]
@@ -13,11 +25,6 @@ public class ExifTool {
         self.metadatas = [:] 
     }
 
-    static func read(fromurl:URL) -> ExifTool {
-        let exif = ExifTool(filepath:fromurl.path)
-        exif.fillMetataData()
-        return exif
-    }
 
     private func fillMetataData() {
         metadatas["FilePath"]=filepath
@@ -33,19 +40,17 @@ public class ExifTool {
         }
         catch {
             print("ERROR")
+            task.terminate()
         }
 
         let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(decoding: outputData, as: UTF8.self)        
-        print(output)
         
         for lines in output.split(separator: "\n") {
             let cols = lines.split(separator: ":")
             let key = String(cols[0]).trimmingCharacters(in: .whitespacesAndNewlines)
             metadatas[key]=String(cols[1]).trimmingCharacters(in: .whitespacesAndNewlines)
         }
-        
-
     }
 
     subscript(string:String) -> String? {
