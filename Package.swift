1331995a328a5b01254167aac080938e44cebc3a
Hervé LEMAI <hlemai@hotmail.com>
2021-12-29 09:31:46 +0100

    ad ios Support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ExifTool",
     platforms: [
-        .macOS(.v11) ],
+        .macOS(.v11),.iOS(.v14) ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
